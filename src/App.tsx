1d8d3f22002b93de45fede0cae39eae19c71c962
Roman Kukhalashvili <cyberdna12@gmail.com>
2024-02-20 13:41:12 +0400

    add desktop header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import HeaderMobile from "./components/HeaderMobile";
 import data from "./data.json";
 import ProductPage from "./components/ProductPage";
+import Header from "./components/Header";
 
 function App() {
   const [websiteData, setWebsiteData] = useState<any>(data);
   return (
     <>
-      <HeaderMobile />
+      {/* <HeaderMobile /> */}
+      <Header />
       <ProductPage />
     </>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 35px 165px;
+`;
+
+const Ul = styled.ul`
+  display: flex;
+  color: #fff;
+  list-style: none;
+  gap: 34px;
+`;
+const Li = styled.li`
+  color: #fff;
+  font-family: Manrope;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 25px; /* 192.308% */
+  letter-spacing: 2px;
+  text-transform: uppercase;
+`;
+function Header() {
+  return (
+    <>
+      <header style={{ backgroundColor: "#0E0E0E" }}>
+        <Nav>
+          <img src="../../assets/shared/desktop/logo.svg" width="143px" />
+          <Ul>
+            <Li>Home</Li>
+            <Li>Headphones</Li>
+            <Li>Speakers</Li>
+            <Li>Earphones</Li>
+          </Ul>
+          <img
+            src="../../assets/shared/desktop/icon-cart.svg"
+            width="23px"
+            height="30px"
+          />
+        </Nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
